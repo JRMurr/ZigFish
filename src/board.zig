ee815c16fc043aec7f836c51ee65de830c25c88a
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-07-06 11:51:20 -0400

    pre-compute max number of cells to the edge of the edge at each cell

--- src/board.zig
+++ src/board.zig
@@ -13,7 +13,66 @@ pub const Position = struct {
         std.debug.assert(self.file < 8);
         return self.rank * 8 + self.file;
     }
+
+    pub fn all_positions() [64]Position {
+        var positions: [64]Position = undefined;
+        inline for (0..8) |rank| {
+            inline for (0..8) |file| {
+                const pos = Position{ .rank = rank, .file = file };
+                positions[pos.to_index()] = pos;
+            }
+        }
+    }
 };
+
+const MoveOffset = enum(i8) {
+    North = 8,
+    South = -8,
+    West = -1,
+    East = 1,
+    NorthWest = 7,
+    NorthEast = 9,
+    SouthWest = -9,
+    SouthEast = -7,
+};
+
+const dir_offsets = [8]MoveOffset{
+    MoveOffset.North,
+    MoveOffset.South,
+    MoveOffset.West,
+    MoveOffset.East,
+    MoveOffset.NorthWest,
+    MoveOffset.NorthEast,
+    MoveOffset.SouthWest,
+    MoveOffset.SouthEast,
+};
+
+fn compute_num_cells_to_edge() [64][8]u8 {
+    const all_positon = Position.all_positions();
+    var dist_to_edge: [64][8]u8 = undefined;
+    for (all_positon) |pos| {
+        const num_north = 7 - pos.rank;
+        const num_south = pos.rank;
+        const num_west = pos.file;
+        const num_east = 7 - pos.file;
+
+        dist_to_edge[pos.to_index()] = .{
+            num_north,
+            num_south,
+            num_west,
+            num_east,
+
+            @min(num_north, num_west),
+            @min(num_north, num_east),
+            @min(num_south, num_west),
+            @min(num_south, num_west),
+        };
+    }
+
+    return num_squares_to_edge;
+}
+
+const num_squares_to_edge = compute_num_cells_to_edge();
 
 pub const Board = struct {
     // TODO: track castling + en passant
@@ -35,4 +94,16 @@ pub const Board = struct {
     pub fn set_cell(self: *Board, pos: Position, cell: Cell) void {
         self.cells[pos.to_index()] = cell;
     }
+
+    // pub fn get_valid_moves(self: Board, pos: Position) anyerror!std.ArrayList(Position) {
+    //     var valid_pos = std.ArrayList(Position);
+
+    //     const cell = self.get_cell(pos);
+    //     switch (cell) {
+    //         .piece => |p| {},
+    //         .empty => {},
+    //     }
+
+    //     return valid_pos;
+    // }
 };
