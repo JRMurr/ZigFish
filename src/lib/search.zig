6ee1d7eb7c36b2887c800cfc99d67913349d9738
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-07-21 14:16:19 -0400

    comment out depth check logs

--- src/lib/search.zig
+++ src/lib/search.zig
@@ -401,7 +401,7 @@ pub fn iterativeSearch(self: *Self, move_allocator: Allocator, max_depth: usize)
     self.best_move = null;
 
     for (1..max_depth) |depth| {
-        std.log.debug("checking at depth: {}", .{depth});
+        // std.log.debug("checking at depth: {}", .{depth});
         self.diagnostics.num_nodes_analyzed = 0;
         const generated_moves = try self.move_gen.getAllValidMoves(move_allocator, false);
         const moves = generated_moves.moves;
@@ -421,12 +421,12 @@ pub fn iterativeSearch(self: *Self, move_allocator: Allocator, max_depth: usize)
                 self.best_score = eval;
             }
             if (self.stop_search.isSet()) {
-                std.log.debug("Check before stopping: {}", .{self.diagnostics.num_nodes_analyzed});
+                // std.log.debug("Check before stopping: {}", .{self.diagnostics.num_nodes_analyzed});
                 self.search_done.set();
                 return self.best_move;
             }
         }
-        std.log.debug("Checked this iteration: {}", .{self.diagnostics.num_nodes_analyzed});
+        // std.log.debug("Checked this iteration: {}", .{self.diagnostics.num_nodes_analyzed});
     }
     self.search_done.set();
     return self.best_move;
