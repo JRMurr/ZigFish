076854550a10b5df35ecad5c6b79728c871071de
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-07-21 17:31:31 -0400

    runFast script will build the old engine if needed

--- runFast.sh
+++ runFast.sh
@@ -6,6 +6,9 @@ mkdir -p ./fastchess-out
 rm -f ./fastchess-out/log
 rm -f ./fastchess-out/pgnout
 
+zig build
+OLD_ENGINE=$(buildAtCommit "3d62d81a61e891f606bf8ce457dd9e4f2a5387ab")
+
 
 fast-chess -maxmoves 100 -concurrency 20 \
     -log file=./fastchess-out/log level=info realtime=true \
@@ -15,4 +18,4 @@ fast-chess -maxmoves 100 -concurrency 20 \
     -draw movenumber=30 movecount=8 score=80 \
     -openings file=./test.pgn format=pgn order=random \
     -engine cmd=./zig-out/bin/zigfish-uci name=new st=0.1 timemargin=100 \
-    -engine cmd=$1 name=old st=0.1 timemargin=100
+    -engine cmd="$OLD_ENGINE" name=old st=0.1 timemargin=100
